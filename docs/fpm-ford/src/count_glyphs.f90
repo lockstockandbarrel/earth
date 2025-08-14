4b052d79fc81278d157d4d237ce23b5120b188ca
lockstockandbarrel <165221663+lockstockandbarrel@users.noreply.github.com>
2025-08-14 20:45:59 +0000

    Deploy FORD docs

--- docs/fpm-ford/src/count_glyphs.f90
+++ docs/fpm-ford/src/count_glyphs.f90
@@ -1,32 +1,53 @@
 program count_glyphs
-! @(#) read a utf-8 file and write it out with lines prefixed with glyph count of the line
-use, intrinsic :: iso_fortran_env, only : output_unit, input_unit
+! @(#) read a utf-8 file and write it out with lines prefixed with glyph counts of the line
+use, intrinsic :: iso_fortran_env, only : stdout=>output_unit, stdin=>input_unit
 implicit none
 intrinsic selected_char_kind
 intrinsic is_iostat_end
 intrinsic len_trim
-integer, parameter                     :: ucs4 = selected_char_kind ('ISO_10646')
-character(len=*),parameter             :: g= '(*(g0))'
-integer                                :: length
-integer                                :: i
-integer                                :: iostat
-character(len=4096,kind=ucs4)          :: uline ! specifies maximum line length of 4096 bytes,
-                                                ! which might be as few as 1024 (ie. 4096/4) glyphs
-character(len=255)                     :: iomsg
+!------
+! NOTE: we will be using the kind name "ucs4" for Unicode variables
+integer, parameter            :: ucs4 = selected_char_kind ('ISO_10646')
+!------
+character(len=*),parameter    :: g= '(*(g0))'
+integer                       :: length
+integer                       :: i
+integer                       :: iostat
+!------
+! NOTE: this character variable is the Unicode kind, not ASCII
+character(len=1024,kind=ucs4) :: uline ! specifies maximum line length of 1024 glyphs
+                                       ! which would be 4*1024 bytes
+!------
+character(len=255)            :: iomsg
 
-   open (input_unit, encoding='UTF-8')
-   open (output_unit, encoding='UTF-8')
+   !------
+   ! NOTE: you can change the encoding used for a file dynamically, even on pre-assigned files
+   open (stdin, encoding='UTF-8')
+   open (stdout, encoding='UTF-8')
+   !------
 
    do
-      read(input_unit,'(a)',iostat=iostat,iomsg=iomsg)uline
+      read(stdin,'(a)',iostat=iostat,iomsg=iomsg)uline
       if(iostat.eq.0)then
+         !------
+         ! NOTE: LEN_TRIM() works with UCS-4 just as with ASCII
          length=len_trim(uline)
-         write(output_unit,'(i9,": ",a)')length,uline(:length)
+         !------
+         !------
+         ! NOTE: String substrings work just like with ASCII as well
+         write(stdout,'(i9,": ",a)')length,uline(:length)
+         !------
       elseif(is_iostat_end(iostat))then
          exit
-      else 
-         ! does the message have to be converted to ucs-4?
-         write(output_unit,g)'<ERROR>',trim(iomsg)
+      else
+         !------
+         ! NOTE:
+         ! does the ASCII message have to be converted to UCS-4?
+         ! This will be discussed in detail later, but for now
+         ! remember you can change the encoding of a file dynamically
+         open (stdout, encoding='DEFAULT')
+         !------
+         write(stdout,g)'<ERROR>',trim(iomsg)
          exit
       endif
    enddo
